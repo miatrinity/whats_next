1862a193a0406e68d6e7cd468ea6e9efda9bccc7
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-01-21 18:17:36 +0100

    User chooses file when creating identity; filename shows up

---
+++ spec/system/user/chooses_file_when_creating_identity_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'User chooses file when creating identity', type: :system do
+  it 'filename shows up' do
+    login_as create(:user)
+
+    visit new_identity_path
+
+    attach_file 'identity[avatar]',
+                Rails.root.join('spec', 'fixtures', 'identity.png'),
+                make_visible: true
+
+    expect(page).to have_css('.file-name', text: 'identity.png', count: 1)
+  end
+end

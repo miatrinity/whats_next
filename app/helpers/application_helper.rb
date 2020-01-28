1e70db29949bc9734ca2e6e5aef6595b2a7b6d52
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-01-28 16:37:06 +0100

    Moved avatar helper to application_helper as it's used by multiple views now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@
 
 # :nodoc:
 module ApplicationHelper
+  def avatar_name(record)
+    if record.avatar.attached?
+      record.avatar.filename
+    else
+      'No file chosen'
+    end
+  end
+
   def new_record_path(record_name)
     public_send "new_#{record_name}_path"
   end
--- app/helpers/identities_helper.rb
+++ app/helpers/identities_helper.rb
@@ -2,11 +2,4 @@
 
 # :nodoc:
 module IdentitiesHelper
-  def avatar_name(identity)
-    if identity.avatar.attached?
-      identity.avatar.filename
-    else
-      'No file chosen'
-    end
-  end
 end
